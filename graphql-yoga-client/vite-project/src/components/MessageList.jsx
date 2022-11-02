1ad92590d0bf801fef68c2d312d0bce657748ab8
dashfuentes <smis117112@gmail.com>
2022-11-01 19:53:16 -0600

    create message list logic

--- graphql-yoga-client/vite-project/src/components/MessageList.jsx
+++ graphql-yoga-client/vite-project/src/components/MessageList.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
 
+export const GET_MESSAGES = gql`
+	{
+		getMessages {
+			_id
+			title
+			author
+			content
+		}
+	}
+`;
 export default function MessageList() {
-  return (
-    <div>MessageForm</div>
-  )
+	const { error, data } = useQuery(GET_MESSAGES);
+
+	if (error) return <h1>Error: {{ error }}</h1>;
+	if (data) {
+		console.log(data);
+	}
+
+	return (
+		<div className="row">
+			<div className="col-md-6 offset-md-3">
+				{data &&
+					data.getMessages.map(({ _id, title, author, content }) => (
+						<div className="card m-2" data-id={_id}>
+							<div className="card-body">
+								<h4>{title}</h4>
+								<h4>{author}</h4>
+								<h4>{content}</h4>
+							</div>
+							<button className="btn btn-danger">Delete</button>
+						</div>
+					))}
+			</div>
+		</div>
+	);
 }
--- graphql-yoga-client/vite-project/src/main.jsx
+++ graphql-yoga-client/vite-project/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient( {
+  cache: new InMemoryCache(),
+  uri: "http://localhost:3100"
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider> 
   </React.StrictMode>
 )
